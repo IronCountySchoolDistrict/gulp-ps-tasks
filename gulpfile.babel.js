63769ba780d11e23e27f6995e685f689bf3d0ec7
Nathan Jones <nathan.jones@ironmail.org>
2016-06-02 10:01:34 -0600

    js-beautify on js, pass in babel plugins instead of in .babelrc which was causing build issues with es6 modules, and updated modules in package.json.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,30 @@
-console.log('hi');
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import runSequence from 'run-sequence';
-
 import del from 'del';
 
 const plugins = gulpLoadPlugins();
 
 gulp.task('build:copy', () =>
-    gulp.src([
-        'LICENSE',
-        'README.md',
-        'package.json',
-        '.config.example.json'
-    ])
-    .pipe(gulp.dest('dist'))
+  gulp.src([
+    'LICENSE',
+    'README.md',
+    'package.json',
+    '.config.example.json'
+  ])
+  .pipe(gulp.dest('dist'))
 );
 
 gulp.task('build:babel', () =>
-    gulp.src('index.js')
-    .pipe(plugins.babel())
-    .pipe(gulp.dest('dist'))
+  gulp.src('index.js')
+  .pipe(plugins.babel())
+  .pipe(gulp.dest('dist'))
 );
 
 gulp.task('clean', () =>
-    del(['dist/*'])
+  del(['dist/*'])
 );
 
 gulp.task('build', (callback) =>
-    runSequence('clean', ['build:copy', 'build:babel'], callback)
+  runSequence('clean', ['build:copy', 'build:babel'], callback)
 );
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import minimist from 'minimist';
 import del from 'del';
 import lazypipe from 'lazypipe';
 import normalize from 'normalize-path';
-import { readFileSync } from 'fs';
+import {
+  readFileSync
+} from 'fs';
 
 const knownOptions = {
   string: ['env', 'config']
@@ -14,25 +16,23 @@ const options = minimist(process.argv.slice(2), knownOptions);
 
 /**
  * Returns a config object by checking three sources in this order:
- *  1. If there was a --config option passed in the cli options, use the 
+ *  1. If there was a --config option passed in the cli options, use the
  *  config.json file provided there.
  *  2. If 1 failed, and there is a config.json file in the project folder, use that config.json
  *  3. If 1 and 2 failed, and there is an environment variable PSTASKS_ROOT, use the config.json in that directory.
  *  4. If the first 3 failed, throw an error.
- *  
+ *
  * @return {object|null}
  */
 function loadConfig() {
   if (options.config) {
     console.log(`Using config.json found at ${options.config}`);
     const normalizedPath = normalize(options.config);
-    const configStr = readFileSync(`${normalizedPath}/config.json`)
-      .toString();
+    const configStr = readFileSync(`${normalizedPath}/config.json`).toString();
     return JSON.parse(configStr);
   }
   try {
-    const configStr = readFileSync('./config.json')
-      .toString();
+    const configStr = readFileSync('./config.json').toString();
     console.log('Using config.json found in project folder');
     return JSON.parse(configStr);
   } catch (e) {
@@ -60,9 +60,9 @@ function loadConfig() {
  * registers the following gulp tasks to the project-level (local) gulp
  * @param  {object} gulp default export
  * @param  {string} __dirname of the project folder that gulp command was executed from
- * @return {undefined} 
+ * @return {null}
  */
-export default function (gulp, projectPath) {
+export default function(gulp, projectPath) {
   const config = loadConfig();
   if (!options.env) {
     options.env = config.default_deploy_target;
@@ -75,86 +75,91 @@ export default function (gulp, projectPath) {
   }
 
   // No image server
-  gulp.task("build-plugin", () =>
-    gulp.src(["plugin/**", "plugin.xml"])
-    .pipe(gulp.dest("dist"))
+  gulp.task('build-plugin', () =>
+    gulp.src(['plugin/**', 'plugin.xml'])
+    .pipe(gulp.dest('dist'))
   );
 
-  gulp.task("build-src", ["build-plugin"], () =>
-    gulp.src("src/**")
-    .pipe(gulp.dest("dist/web_root"))
+  gulp.task('build-src', ['build-plugin'], () =>
+    gulp.src('src/**')
+    .pipe(gulp.dest('dist/web_root'))
   );
 
-  gulp.task("zip", ["build-src"], () =>
-    gulp.src("dist")
-    .pipe(plugins.zip("plugin.zip"))
-    .pipe(gulp.dest("dist"))
+  gulp.task('zip', ['build-src'], () =>
+    gulp.src('dist')
+    .pipe(plugins.zip('plugin.zip'))
+    .pipe(gulp.dest('dist'))
   );
 
-  gulp.task("clean", ["zip"], () => {
-    del(["dist/*", "!dist/*.zip"])
+  gulp.task('clean', ['zip'], () => {
+    del(['dist/*', '!dist/*.zip'])
   });
 
-  gulp.task("build-no-img", ["build-plugin", "build-src", "zip", "clean"])
+  gulp.task('build-no-img', ['build-plugin', 'build-src', 'zip', 'clean'])
 
-  gulp.task("deploy", () =>
-    gulp.src("dist/src/**")
+  gulp.task('deploy', () =>
+    gulp.src('dist/src/**')
     .pipe(plugins.debug())
     .pipe(deploy())
   );
 
-  gulp.task("watch-deploy", () =>
-    gulp.src("src/**")
-    .pipe(plugins.watch("src/**"))
+  gulp.task('watch-deploy', () =>
+    gulp.src('src/**')
+    .pipe(plugins.watch('src/**'))
     .pipe(preprocess())
     .pipe(deploy())
   );
 
-  gulp.task("build", ["less", "build-static", "babel"], () =>
+  gulp.task('build', ['less', 'build-static', 'babel'], () =>
     gulp.src([
-      "./plugin/**/*",
-      "./src/**/*",
-      "./queries_root/**/*",
-      "plugin/plugin.xml",
-      "!src/**/*.less",
-      "!src/**/*.{png,gif,jpg,bmp,swf,js}",
-      "!src/**/ext/**",
-      "!src/less{,/**}"
+      './plugin/**/*',
+      './src/**/*',
+      './queries_root/**/*',
+      'plugin/plugin.xml',
+      '!src/**/*.less',
+      '!src/**/*.{png,gif,jpg,bmp,swf,js}',
+      '!src/**/ext/**',
+      '!src/less{,/**}'
     ], {
-      base: "./"
+      base: './'
     })
     .pipe(plugins.debug())
     .pipe(preprocess())
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest('dist'))
   );
 
-  gulp.task("build-static", () =>
+  gulp.task('build-static', () =>
     gulp.src([
-      "./src/**/*.{jpg,png,gif,bmp,swf}",
-      "./src/**/ext/**"
+      './src/**/*.{jpg,png,gif,bmp,swf}',
+      './src/**/ext/**'
     ], {
-      base: "./"
+      base: './'
     })
-    .pipe(gulp.dest("dist/"))
+    .pipe(gulp.dest('dist/'))
   );
 
-  gulp.task("package", () =>
-    gulp.src("dist/plugin/**")
-    .pipe(plugins.zip("plugin.zip"))
-    .pipe(gulp.dest("dist"))
+  gulp.task('package', () =>
+    gulp.src('dist/plugin/**')
+    .pipe(plugins.zip('plugin.zip'))
+    .pipe(gulp.dest('dist'))
   );
 
-  gulp.task("babel", () =>
+  gulp.task('babel', () =>
     gulp.src([
-      "./src/**/*.js",
-      "!src/**/ext/**"
+      './src/**/*.js',
+      '!src/**/ext/**'
     ], {
-      base: "./"
+      base: './'
     })
     .pipe(plugins.debug())
     .pipe(preprocess())
-    .pipe(plugins.babel())
-    .pipe(gulp.dest("dist"))
+    .pipe(plugins.babel({
+      plugins: [
+        'transform-es2015-modules-amd',
+        'transform-es2015-classes'
+      ]
+    }))
+    .pipe(gulp.dest('dist'))
   );
 
   const deploy = lazypipe()
@@ -174,20 +179,20 @@ export default function (gulp, projectPath) {
       }));
     });
 
-  gulp.task("lint", () =>
-    gulp.src("src/**/*.js")
+  gulp.task('lint', () =>
+    gulp.src('src/**/*.js')
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format())
   );
 
-  gulp.task("less", () =>
+  gulp.task('less', () =>
     gulp.src([
-      "src/**/*.less",
-      "!src/**/ext/**",
-      "!src/**/less/**"
+      'src/**/*.less',
+      '!src/**/ext/**',
+      '!src/**/less/**'
     ])
     .pipe(plugins.less())
     .pipe(preprocess())
-    .pipe(gulp.dest("dist/src"))
+    .pipe(gulp.dest('dist/src'))
   );
 }
